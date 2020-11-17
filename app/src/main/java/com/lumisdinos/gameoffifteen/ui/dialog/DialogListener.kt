9316b83d4ea796859a0277b7f3c5e1e4e544ab7b
LumisD <lumisdinos@gmail.com>
2020-11-17 11:31:50 -0300

    Delete unused DialogListener

--- app/src/main/java/com/lumisdinos/gameoffifteen/ui/dialog/DialogListener.kt
+++
@@ -1,7 +0,0 @@
-package com.lumisdinos.gameoffifteen.ui.dialog
-
-interface DialogListener {
-    fun onPositiveDialogClick(result: List<String>)//action; string/s
-    fun onNegativeDialogClick(result: List<String>)//action
-    fun onNeutralDialogClick(result: List<String>)//action
-}
